8414fcaef459ea14dba50094b13ba61c81da086c
algasami <105358814+algasami@users.noreply.github.com>
2024-06-28 21:19:52 +0800

    basic graph implementation and application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use dsa_rs::graph::Graph;
+use text_io::read;
+
 fn main() {
-    println!("Hello, world!");
+    let mut g: Graph<i32> = Graph::new();
+
+    println!("Input <nodes> <edges> on the next line");
+    let nodes: usize = read!();
+    let edges: usize = read!();
+    for _i in 0..nodes {
+        g.add_node(0);
+    }
+    for _e in 0..edges {
+        let key1: usize = read!();
+        let key2: usize = read!();
+        match g.add_edge(key1, key2) {
+            Err(_) => {
+                println!("Add edge exception!");
+            }
+            Ok(()) => {}
+        }
+    }
+    println!("{}", g);
 }
